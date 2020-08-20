cd5913217e0ad1f15742bdc9528f7f22970fca85
Oliver Steiner <oliver@mollo.ch>
2020-08-20 23:27:07 +0200

    Add media entity Formatter

--- src/Plugin/Field/FieldFormatter/LightSlideGalleryMedia.php
+++ src/Plugin/Field/FieldFormatter/LightSlideGalleryMedia.php
@@ -3,9 +3,12 @@
 namespace Drupal\light_slide_gallery\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
+use Drupal\Core\Render\RendererInterface;
 use Drupal\file\Entity\File;
 use Drupal\file\FileInterface;
 use Drupal\image\Entity\ImageStyle;
+use Drupal\image\ImageStyleStorageInterface;
+use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatter;
 use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatterBase;
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Field\FieldItemListInterface;
@@ -19,17 +22,17 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Cache\Cache;
 
 /**
- * Plugin implementation of the 'light_slide_gallery_image_formatter' formatter.
+ * Plugin implementation of the 'light_slide_gallery_media_formatter' formatter.
  *
  * @FieldFormatter(
- *   id = "light_slide_gallery_image_formatter",
- *   label = @Translation("Lightslide & LightGallery Formatter"),
+ *   id = "light_slide_gallery_media_formatter",
+ *   label = @Translation("Lightslide & LightGallery Media Formatter"),
  *   field_types = {
- *     "image",
+ *     "entity_reference",
  *   }
  * )
  */
-class LightSlideGalleryFormatter extends ImageFormatterBase
+class LightSlideGalleryMedia extends ImageFormatter
 {
   /**
    * The current user.
@@ -53,7 +56,14 @@ class LightSlideGalleryFormatter extends ImageFormatterBase
   protected $imageStyleStorage;
 
   /**
-   * Constructs a new LightSlideGalleryFormatter object.
+   * The renderer service.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
+   * Constructs an MediaThumbnailFormatter object.
    *
    * @param string $plugin_id
    *   The plugin_id for the formatter.
@@ -71,10 +81,11 @@ class LightSlideGalleryFormatter extends ImageFormatterBase
    *   Any third party settings settings.
    * @param \Drupal\Core\Session\AccountInterface $current_user
    *   The current user.
-   * @param \Drupal\Core\Utility\LinkGeneratorInterface $link_generator
-   *   The link generator service.
-   * @param \Drupal\Core\Entity\EntityStorageInterface $image_style_storage
-   *   The entity storage for the image.
+   * @param \Drupal\image\ImageStyleStorageInterface $image_style_storage
+   *   The image style entity storage handler.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer service.
+   * @param LinkGeneratorInterface $link_generator
    */
   public function __construct(
     $plugin_id,
@@ -85,8 +96,10 @@ class LightSlideGalleryFormatter extends ImageFormatterBase
     $view_mode,
     array $third_party_settings,
     AccountInterface $current_user,
-    LinkGeneratorInterface $link_generator,
-    EntityStorageInterface $image_style_storage
+    ImageStyleStorageInterface $image_style_storage,
+    RendererInterface $renderer,
+    LinkGeneratorInterface $link_generator
+
   ) {
     parent::__construct(
       $plugin_id,
@@ -95,8 +108,11 @@ class LightSlideGalleryFormatter extends ImageFormatterBase
       $settings,
       $label,
       $view_mode,
-      $third_party_settings
+      $third_party_settings,
+      $current_user,
+      $image_style_storage
     );
+    $this->renderer = $renderer;
     $this->currentUser = $current_user;
     $this->linkGenerator = $link_generator;
     $this->imageStyleStorage = $image_style_storage;
@@ -120,8 +136,10 @@ class LightSlideGalleryFormatter extends ImageFormatterBase
       $configuration['view_mode'],
       $configuration['third_party_settings'],
       $container->get('current_user'),
-      $container->get('link_generator'),
-      $container->get('entity_type.manager')->getStorage('image_style')
+      $container->get('entity_type.manager')->getStorage('image_style'),
+      $container->get('renderer'),
+            $container->get('link_generator')
+
     );
   }
 
@@ -132,7 +150,8 @@ class LightSlideGalleryFormatter extends ImageFormatterBase
    * of type \Drupal\file\Plugin\Field\FieldType\FileItem and calls
    * isDisplayed() which is not in FieldItemInterface.
    */
-  protected function needsEntityLoad(EntityReferenceItem $item) {
+  protected function needsEntityLoad(EntityReferenceItem $item)
+  {
     return !$item->hasNewEntity();
   }
 
@@ -142,11 +161,11 @@ class LightSlideGalleryFormatter extends ImageFormatterBase
   public static function defaultSettings()
   {
     return [
-      'image_style_default' => '',
-      'image_style_thumbnail' => '',
-      'image_style_fullscreen' => '',
-      'gallery_style' => 0,
-      'image_link' => '',
+      'image_style_default' => 'medium',
+      'image_style_thumbnail' => 'unig_thumbnail',
+      'image_style_fullscreen' => 'unig_hd',
+      'gallery_style' => 'grid',
+      'image_link' => ''
     ] + parent::defaultSettings();
   }
 
@@ -165,16 +184,16 @@ class LightSlideGalleryFormatter extends ImageFormatterBase
       '#type' => 'select',
       '#default_value' => $this->getSetting('image_style_default'),
       '#empty_option' => t('None (original image)'),
-      '#options' => $image_styles,
+      '#options' => $image_styles
     ];
 
-    // Tumbnail
+    // Thumbnail
     $element['image_style_thumbnail'] = [
       '#title' => t('Image style Thumbnail'),
       '#type' => 'select',
       '#default_value' => $this->getSetting('image_style_thumbnail'),
       '#empty_option' => t('None (original image)'),
-      '#options' => $image_styles,
+      '#options' => $image_styles
     ];
 
     // Fullscreen
@@ -191,8 +210,8 @@ class LightSlideGalleryFormatter extends ImageFormatterBase
         ),
         '#access' => $this->currentUser->hasPermission(
           'administer image styles'
-        ),
-      ],
+        )
+      ]
     ];
 
     // Gallery Style
@@ -200,7 +219,7 @@ class LightSlideGalleryFormatter extends ImageFormatterBase
       '#title' => t('Gallery Style'),
       '#type' => 'select',
       '#default_value' => $this->getSetting('gallery_style'),
-      '#options' => $gallery_styles,
+      '#options' => $gallery_styles
     ];
 
     return $element;
@@ -231,7 +250,7 @@ class LightSlideGalleryFormatter extends ImageFormatterBase
     $gallery_style_setting = $this->getSetting('gallery_style');
     if (isset($gallery_style_setting)) {
       $summary[] = t('Gallery style: @style', [
-        '@style' => $gallery_styles[$gallery_style_setting],
+        '@style' => $gallery_styles[$gallery_style_setting]
       ]);
     }
     return $summary;
@@ -244,73 +263,66 @@ class LightSlideGalleryFormatter extends ImageFormatterBase
   {
     $elements = [];
     $images = [];
-    $url = null;
-    $files = $this->getEntitiesToView($items, $langcode);
     $node = $items->getEntity();
     $nid = $node->id();
 
-    // Collect cache tags to be added for each item in the field.
-    $base_cache_tags = [];
+    $media_items = $this->getEntitiesToView($items, $langcode);
 
     // Early opt-out if the field is empty.
-    if (empty($files)) {
+    if (empty($media_items)) {
       return $elements;
     }
 
+    $image_style_setting = $this->getSetting('image_style');
     // Generate ID for js call
     $field = $items->getName();
     $gallery_style = $this->getSetting('gallery_style');
 
     $slide_id = 'lightSlideGallery' . '-' . $nid . '-' . $field;
     $slide_id = str_replace(array('_', ' '), '-', $slide_id);
-
-    // Image Style
-    $image_style_default = $this->getSetting('image_style_default');
-    $image_style_thumbnail = $this->getSetting('image_style_thumbnail');
-    $image_style_fullscreen = $this->getSetting('image_style_fullscreen');
-
-    // Loop over all images in field
     $cache_contexts = [];
-    foreach ($files as $delta => $file) {
 
-      $image_default = self::createImageStyle($file, $image_style_default);
-      $image_thumbnail = self::createImageStyle($file, $image_style_thumbnail);
-      $image_fullscreen = self::createImageStyle(
-        $file,
-        $image_style_fullscreen
-      );
+    /** @var \Drupal\media\MediaInterface[] $media_items */
+    foreach ($media_items as $delta => $media) {
+      // Get ImageStyles from Settings
+      $image_style_default = $this->getSetting('image_style_default');
+      $image_style_thumbnail = $this->getSetting('image_style_thumbnail');
+      $image_style_fullscreen = $this->getSetting('image_style_fullscreen');
+
+      // Load image styles
+      $fid = $media->field_media_image->target_id;
+
+      $image_default = self::createImageStyle($fid, $image_style_default);
+      $image_thumbnail = self::createImageStyle($fid, $image_style_thumbnail);
+      $image_fullscreen = self::createImageStyle($fid, $image_style_fullscreen);
 
       $images[$delta]['default'] = $image_default;
       $images[$delta]['thumbnail'] = $image_thumbnail;
       $images[$delta]['fullscreen'] = $image_fullscreen;
+    }
 
-
-    // Extract field item attributes for the theme function, and unset them
-    // from the $item so that the field template does not re-render them.
-    $item = $file->_referringItem;
-    $item_attributes = $item->_attributes;
-
-      unset($item->_attributes);
-  }
-      $cache_tags = Cache::mergeTags($base_cache_tags, $file->getCacheTags());
     $elements[] = [
       '#theme' => 'light_slide_gallery',
-      '#item' => $item,
-      '#item_attributes' => $item_attributes,
       '#images' => $images,
 
       '#slide_id' => $slide_id,
-      '#gallery_style' => $gallery_style,
-      '#cache' => [
-        'tags' => $cache_tags,
-        'contexts' => $cache_contexts,
-      ],
+      '#gallery_style' => $gallery_style
     ];
 
     $elements['#attached']['library'][] =
       'light_slide_gallery/light_slide_gallery.main';
 
     return $elements;
+  }
+
+  public static function gallery_styles_options()
+  {
+    return [
+      'slider' => 'Slider',
+      'grid' => 'Grid',
+      'animated_grid' => 'Animated Grid',
+      'single_image' => 'Single Image'
+    ];
   }
 
   public static function createImageStyle(
@@ -354,15 +366,5 @@ class LightSlideGalleryFormatter extends ImageFormatterBase
       }
     }
     return $image;
-  }
-
-  public static function gallery_styles_options()
-  {
-    return [
-      'slider' => 'Slider',
-      'test' => 'Grid',
-      'animated_grid' => 'Animated Grid',
-      'single_image' => 'Single Image',
-    ];
   }
 }
